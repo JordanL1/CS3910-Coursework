74e895f602773362c024287b78b7672954acf9f5
Jordan Lees <jordan.lees1@gmail.com>
2019-12-11 23:32:40 +0000

    Replace swarm search specific by number of iterations with swarm search methods with time limit; separate methods for LSI and non-LSI; allow cognitive coefficient to be changed for the swarm.

--- Swarm.py
+++ Swarm.py
@@ -1,34 +1,53 @@
+import time
+
 from Pallets import Pallets
 from Particle import Particle
 from Particle_With_LS import Particle_With_LS
 
 class Swarm:
-    def __init__(self, pallet_problem : Pallets, swarm_size : int, iterations : int, is_local_initialised : bool):
-        self.swarm_size = swarm_size
-        self.iterations = iterations
-        self.particles = [None for x in range(swarm_size)]
+    def __init__(self, pallet_problem : Pallets, cognitive_coefficient = 1.1193):        
         self.pallet_problem = pallet_problem
-        self.gbest = self.pallet_problem.generate_random_solution()
-        self.gbest_cost = self.pallet_problem.evaluate_cost(self.gbest)
+        Particle.COGNITIVE_COEFFICIENT = cognitive_coefficient
+        Particle_With_LS.COGNITIVE_COEFFICIENT = cognitive_coefficient
 
-        # Create particles
+    def timed_swarm_search(self, swarm_size: int, run_time: int):
+        end_time = time.time() + run_time
+        gbest = self.pallet_problem.generate_random_solution()
+        gbest_cost = self.pallet_problem.evaluate_cost(gbest)
+        self.particles = [None for x in range(swarm_size)]
 
-        if not is_local_initialised:
-            for i in range(self.swarm_size):
+        for i in range(swarm_size):
                 self.particles[i] = Particle(self.pallet_problem, self.pallet_problem.generate_random_solution())
-        else:
-            for i in range(self.swarm_size):
-                self.particles[i] = Particle_With_LS(self.pallet_problem, self.pallet_problem.generate_random_solution())
 
-    def swarm_search(self):
-        for i in range(self.iterations):
+        while time.time() < end_time:
             for j in range(len(self.particles)):
-                pbest = self.particles[j].update_particle(self.gbest)
+                pbest = self.particles[j].update_particle(gbest)
                 pbest_cost = self.pallet_problem.evaluate_cost(pbest)
 
-                if pbest_cost < self.gbest_cost:
-                    self.gbest = pbest
-                    self.gbest_cost = pbest_cost
+                if pbest_cost < gbest_cost:
+                    gbest = pbest
+                    gbest_cost = pbest_cost
 
         #print(f"Best found by swarm search: {self.gbest} costing {self.gbest_cost}")
-        return [self.gbest, self.gbest_cost]
+        return [gbest, gbest_cost]
+
+    def timed_swarm_search_with_lsi(self, swarm_size: int, run_time: int):
+        end_time = time.time() + run_time
+        gbest = self.pallet_problem.generate_random_solution()
+        gbest_cost = self.pallet_problem.evaluate_cost(gbest)
+        self.particles = [None for x in range(swarm_size)]
+
+        for i in range(swarm_size):
+                self.particles[i] = Particle_With_LS(self.pallet_problem, self.pallet_problem.generate_random_solution())
+
+        while time.time() < end_time:
+            for j in range(len(self.particles)):
+                pbest = self.particles[j].update_particle(gbest)
+                pbest_cost = self.pallet_problem.evaluate_cost(pbest)
+
+                if pbest_cost < gbest_cost:
+                    gbest = pbest
+                    gbest_cost = pbest_cost
+
+        #print(f"Best found by swarm search: {self.gbest} costing {self.gbest_cost}")
+        return [gbest, gbest_cost]
